a32feb080b847b360282d00de3e6448fa2d72148
maria <maria.icanoca@888.ru>
2018-08-24 12:33:18 +0300

    maxpprod

--- interviewbit/arrays.go
+++ interviewbit/arrays.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	//test min-steps-in-infinite-grid
-	fmt.Println(arrays.CoverPoints([]int{0, 1, 1}, []int{0, 1, 2}))
-	fmt.Println(arrays.CoverPoints([]int{0, 1, 3}, []int{0, 2, 2}))
+	fmt.Println(arrays.MaxSpecialProduct([]int{5, 9, 6, 8, 6, 4, 6, 9, 5, 4, 9 }))
+	fmt.Println(arrays.MaxSpecialProduct([]int{6, 7, 9, 5, 5, 8 }))
+	fmt.Println(arrays.MaxSpecialProduct([]int{7, 5, 7, 9, 8, 7}))
 }
--- interviewbit/arrays/maxspprod.go
+++ interviewbit/arrays/maxspprod.go
@@ -1,1 +1,53 @@
 package arrays
+
+func MaxSpecialProduct (A []int )  (int)  {
+	rv := make([]int, 0)
+	ri := make([]int, 0)
+	lv := make([]int, 0)
+	li := make([]int, 0)
+
+	maxL := make([]int, len(A))
+	max := make([]int, len(A))
+
+	for i, v := range A {
+		if len(lv) > 0 {
+			for j := len(lv) - 1; j >= 0; j-- {
+				if lv[j] > v {
+					maxL[i] = li[j]
+					break
+				}
+			}
+		}
+
+		lv = append(lv, v)
+		li = append(li, i)
+	}
+
+	for i := len(A) - 1; i >= 0 ; i-- {
+		if len(rv) > 0 && maxL[i] > 0 {
+			for j := len(rv) - 1; j >= 0; j-- {
+				if rv[j] > A[i] {
+					max[i] = ri[j] * maxL[i]
+					break
+				}
+			}
+		}
+		rv = append(rv, A[i])
+		ri = append(ri, i)
+	}
+
+	return maxInArray(max) % 1000000007
+
+
+}
+
+func maxInArray(A []int) int {
+	max := 0
+	for _, v := range A{
+		if v > max {
+			max = v
+		}
+	}
+
+	return max
+}
