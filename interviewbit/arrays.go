590795425d080028fd8f737cda1b3badf967f32f
maria <maria.icanoca@888.ru>
2018-09-21 15:17:40 +0300

    largest-number

--- interviewbit/arrays.go
+++ interviewbit/arrays.go
@@ -6,11 +6,16 @@ import (
 )
 
 func main() {
-	//test max-non-negative-subarray
-	fmt.Println(arrays.Maxset([]int{1, 2, 5, -7, 2, 3}))
-	fmt.Println(arrays.Maxset([]int{1, 2, 3, -7, 1, 5}))
-	fmt.Println(arrays.Maxset([]int{1, 2, 3, -7, 3, 1, 2}))
-	fmt.Println(arrays.Maxset([]int{-63216, -75973, -82720, -81169, 15827, -16728, -20384, 89949, 16046, -68666, 60520,
-	41991, -33258, 68744, 85718, -66825, 58689, -73014, 2918, 33029, 6727, 33754, -8396, 3083, -46865, 15252, -77876, -32675, -82710, 28192, -54747}))
+	//test largest-namber
+	fmt.Println(arrays.LargestNumber([]int{3, 30, 34, 5, 9}))
+	fmt.Println(arrays.LargestNumber([]int{3, 33, 34, 5, 9}))
+	fmt.Println(arrays.LargestNumber([]int{3, 32, 34, 5, 9}))
+	fmt.Println(arrays.LargestNumber([]int{101, 3, 32, 34, 5, 9}))
+	fmt.Println(arrays.LargestNumber([]int{301, 3, 32, 34, 5, 9}))
+	fmt.Println(arrays.LargestNumber([]int{29, 298}))
+	fmt.Println(arrays.LargestNumber([]int{29, 2987}))
+	fmt.Println(arrays.LargestNumber([]int{12, 121}))
+	fmt.Println(arrays.LargestNumber([]int{931, 94, 209, 448, 716, 903, 124, 372, 462, 196, 715, 802, 103, 740, 389, 872, 615, 638, 771, 829, 899, 999, 29, 163, 342, 902, 922, 312, 326, 817, 288, 75, 37, 286, 708, 589, 975, 747, 743, 699, 743, 954, 523, 989, 114, 402, 236, 855, 323, 79, 949, 176, 663, 587, 322}))
+	fmt.Println(arrays.LargestNumber([]int{0,0,0}))
 
 }
